623c2abf392ab5eb07ef2f2e6a8ae7f454b51f12
Bilal Mian <bilalj.mian@gmail.com>
2016-05-12 20:53:19 -0400

    port fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var app = express();
-var PORT = env.process.PORT;
+var PORT = process.env.PORT;
 
 app.use(express.static(process.cwd() + '/public'));
 
